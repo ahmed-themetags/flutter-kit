e878d4e248bb9f4c5c7325644c47381ed94011bb
ahmed-themetags <ahmedthemetags@gmail.com>
2025-08-06 01:55:39 +0600

    Stateful widget with count and dartk light mood

--- lib/screens/ui/stateful_widgets_screen.dart
+++ lib/screens/ui/stateful_widgets_screen.dart
@@ -8,193 +8,124 @@ class StatefulWidgetsScreen extends StatefulWidget {
 }
 
 class _StatefulWidgetsScreenState extends State<StatefulWidgetsScreen> {
+  bool _isdarkMood = false;
+  int _counter    = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          color: Colors.orange, // 🔵 Set your background color here
+          color: _isdarkMood ? Colors.black : Colors.blue, // 🔵 Set your background color here
           width: double.infinity,
           height: double.infinity,
           child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 40),
-                Container(
-                  width: 400,
-                  height: 250,
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.orange),
-                    color: Colors.white,
+                SwitchListTile(
+                  title: Text(
+                    _isdarkMood ? 'Dark Mode' : 'Light Mode',
+                    style: TextStyle(
+                      color: _isdarkMood ? Colors.blue : Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 150,
-                            height: 100,
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.red, width: 2), // Border around text
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              '01 Column under Container',
-                              style: TextStyle(fontSize: 16),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          SizedBox(height: 20), // Add space between containers(),
-                          Container(
-                            width: 150,
-                            height: 100,
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.red, width: 2), // Border around text
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              '02 Column under Container',
-                              style: TextStyle(fontSize: 16),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          SizedBox(height: 20), // Add space between containers(),
-                          Container(
-                            width: 150,
-                            height: 100,
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.red, width: 2), // Border around text
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              '03 Column under Container',
-                              style: TextStyle(fontSize: 16),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          SizedBox(height: 20), // Add space between containers(),
-                          Container(
-                            width: 150,
-                            height: 100,
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.red, width: 2), // Border around text
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              '04 Column under Container',
-                              style: TextStyle(fontSize: 16),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
+                  value: _isdarkMood,
+                  onChanged: (val) => setState(() {
+                    _isdarkMood = val;
+                  }),
+                ),
 
-                        ],
+                // Counter
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      _counter.toString(),
+                      style: TextStyle(
+                        color: _isdarkMood ? Colors.grey : Colors.white,
+                        fontSize: 80,
+                        fontWeight: FontWeight.bold,
                       ),
-                    )
-                  ),
-                ),
-                SizedBox(height: 40),
-                Container(
-                  width: 450,
-                  height: 250,
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.orange),
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Stateful Widgets Screen',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
                     ),
                   ),
                 ),
-                SizedBox(height: 40),
-                Container(
-                  width: 150,
-                  height: 150,
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.orange),
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Stateful Widgets Screen',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+
+                SizedBox(
+                  height: 20,
                 ),
-                SizedBox(height: 40),
-                Container(
-                  width: 150,
-                  height: 150,
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.orange),
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Stateful Widgets Screen',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                Container(
-                  width: 150,
-                  height: 150,
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.orange),
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Stateful Widgets Screen',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                Container(
-                  width: 150,
-                  height: 150,
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.orange),
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Stateful Widgets Screen',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Flutter UI Kids',
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.white
+                        ),
+                      ),
+                      Text(
+                        'Developed by Ahmed Ullah',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+
+                      Text(
+                        _counter > 70
+                            ? '🏆' // Champion!
+                            : _counter > 60
+                            ? '🤖' // Superhuman!
+                            : _counter > 50
+                            ? '🦾' // Power mode!
+                            : _counter > 40
+                            ? '🚀' // Flying high!
+                            : _counter > 30
+                            ? '🔥' // On fire!
+                            : _counter > 25
+                            ? '🎯' // On target!
+                            : _counter > 20
+                            ? '😎' // Cool!
+                            : _counter > 15
+                            ? '🙌' // Nice!
+                            : _counter > 10
+                            ? '💪' // Getting strong!
+                            : _counter > 5
+                            ? '✨' // Good start!
+                            : _counter > 0
+                            ? '🌱' // Just started!
+                            : '💤', // Not started yet,
+                        style: TextStyle(
+                          color: _isdarkMood ? Colors.grey : Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+
+                      ElevatedButton(
+                        child: Text(
+                          'Click',
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
+                        onPressed: () => setState(() {
+                          _isdarkMood = true;
+                          _counter  += 1;
+                        }),
+                      ),
+                    ],
+                  )
                 ),
               ],
             ),
